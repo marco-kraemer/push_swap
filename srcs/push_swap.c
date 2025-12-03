c397d7345c9b0e1ce3b3e7016672a5985e9597ac
Marco Santos <msantos2@c3a9c2.42berlin.de>
2025-12-03 17:31:56 +0100

    Push Swap

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,18 +6,50 @@
 /*   By: msantos2 <msantos2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 16:31:15 by msantos2          #+#    #+#             */
-/*   Updated: 2025/12/03 17:19:48 by msantos2         ###   ########.fr       */
+/*   Updated: 2025/12/03 17:31:48 by msantos2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-t_list	*get_numbers(char **argv)
+typedef struct s_stack
 {
-	t_list	*stack;
+	int				nbr;
+	struct s_stack	*next;
+}	t_stack;
+
+t_stack	*create_stack(int nbr)
+{
+	t_stack	*node;
+
+	node = malloc (sizeof(t_stack) * 1);
+	if (!node)
+		return (NULL);
+	node->nbr = nbr;
+	node->next = NULL;
+	return (node);
+}
+
+void	stack_add(t_stack **lst, t_stack *new)
+{
+	t_stack	*last;
+
+	last = *lst;
+	if (*lst == NULL)
+	{
+		*lst = new;
+		return ;
+	}
+	while (last->next != NULL)
+		last = last->next;
+	last->next = new;
+}
+
+t_stack	*get_numbers(char **argv)
+{
+	t_stack	*stack;
 	int		i;
 	int		j;
-	int		nbr;
 
 	i = 0;
 	while (argv[i])
@@ -34,19 +66,17 @@ t_list	*get_numbers(char **argv)
 		}
 		i++;
 	}
-	i = 0;
+	i = 1;
+	stack = NULL;
 	while (argv[i])
-	{
-		nbr = ft_atoi(argv[i++]);
-		ft_lstadd_back(&stack, ft_lstnew(&nbr));
-	}
+		stack_add(&stack, create_stack(ft_atoi(argv[i++])));
 	return (stack);
 }
 
 int	main(int argc, char **argv)
 {
 	int		i;
-	t_list	*stack_a;
+	t_stack	*stack_a;
 
 	if (argc == 1)
 	{
@@ -57,7 +87,7 @@ int	main(int argc, char **argv)
 	i = 0;
 	while (stack_a)
 	{
-		ft_printf("%i\n", stack_a->content);
+		ft_printf("%i\n", stack_a->nbr);
 		stack_a = stack_a->next;
 	}
 	return (1);
